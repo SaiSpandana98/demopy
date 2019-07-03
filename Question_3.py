e8db86f5cdd01428686510a51c1230a69b5531f3
SaiSpandana98 <43847230+SaiSpandana98@users.noreply.github.com>
2019-07-03 16:40:39 +0530

    assignment3

---
+++ Question_3.py
@@ -0,0 +1,16 @@
+def main():
+    order = "rcta"
+    inputArray = ["car", "rat", "cat",]
+
+    lengthArray = len(inputArray)
+    sortStringArray(order, inputArray, lengthArray)
+
+def sortStringArray(order, inputArray, lengthArray):
+    inputArray = sorted(inputArray, key = lambda word: [order.index(s) for s in word])
+    print(inputArray)
+
+if __name__ == "__main__":
+    main()
+
+
+
